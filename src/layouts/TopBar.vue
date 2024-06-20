eeac54f5d74a47362ea1991851bb83a05b5d02c1
Dyno Nguyen <tuannguyentn2504@gmail.com>
2024-06-20 20:51:01 +0700

    Update UI

--- src/layouts/TopBar.vue
+++ src/layouts/TopBar.vue
@@ -1,5 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { APP_NAME } from '~/constants/common'
+import { withAssets } from '~/utils/helpers'
+</script>
 
 <template>
-  <div class="flex-center h-12 border-b border-gray-100 dark:(border-gray-400,border-dashed)"></div>
+  <div class="flex-center h-14 border-b border-gray-200 dark:border-gray-600 px-4">
+    <div class="flex-center space-x-2">
+      <img class="w-8 h-8" :src="withAssets('img/logo.png')" />
+      <div class="text-xl font-semibold">{{ APP_NAME }}</div>
+    </div>
+  </div>
 </template>
